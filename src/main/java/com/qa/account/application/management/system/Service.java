d0d029cbd3c376212c91e0d62dc5dc0a938b72a5
Andrew-Freitas-1106 <andrewfreitas189@gmail.com>
2018-08-08 17:05:53 +0100

    hotfix

--- src/main/java/com/qa/account/application/management/system/Service.java
+++ src/main/java/com/qa/account/application/management/system/Service.java
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-package com.qa.account.application.management.system;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Service {
-	
-	Map<String,Account> bankAccount;
-	
-	public Service() {
-		bankAccount = new HashMap<>();
-	}
-	
-
-	public void addAccount(String key, Account account) {
-		bankAccount.put(key, account);
-		
-	}
-
-	public Account retrieveAccount(String key) {
-		return bankAccount.get(key);
-	}
-
-}
-=======
 package com.qa.account.application.management.system;
 
 import java.util.HashMap;
@@ -54,4 +28,4 @@ public class Service {
 	}
 
 }
->>>>>>> print-account-as-json-branch
+
